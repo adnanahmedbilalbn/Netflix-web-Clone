2cef7fafa0d17a7fc3c91c26f32d21e90464aac8
Adnan Ahmed <adnanahmedbilalbn@gmsil.com>
2024-04-29 17:39:57 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+$(".answer").hide();
+$(".btn-1").on("click",function(){
+    $(".answer-1").toggle();
+});
+$(".btn-2").on("click",function(){
+    $(".answer-2").toggle();
+});
+
+$(".btn-3").on("click",function(){
+    $(".answer-3").toggle();
+});
+
+$(".btn-4").on("click",function(){
+    $(".answer-4").toggle();
+});
+
+$(".btn-5").on("click",function(){
+    $(".answer-5").toggle();
+});
+
+$(".btn-6").on("click",function(){
+    $(".answer-6").toggle();
+});
